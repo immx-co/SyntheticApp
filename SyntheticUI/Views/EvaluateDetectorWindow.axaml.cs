9156a6957685028d7c40282aadc4e1dfe4542d15
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2025-06-05 12:02:05 +0300

    added windows

---
+++ SyntheticUI/Views/EvaluateDetectorWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SyntheticUI;
+
+public partial class EvaluateDetectorWindow : UserControl
+{
+    public EvaluateDetectorWindow()
+    {
+        InitializeComponent();
+    }
+}
